f1185bfa2550ef0aa2b8760fa6f2414d68158c6b
Moises Alfaro <107770920+MoisesAlfar0o@users.noreply.github.com>
2025-02-24 21:50:57 -0600

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,33 @@
+const d = document;
+
+const buttons = d.querySelectorAll('.rating-btns');
+const btnSubmit = d.getElementById('btn-submit');
+const cardRating = d.querySelector('.card-rating');
+const cardThanks = d.querySelector('.card-thanks');
+const outOf = d.getElementById('rating-selected');
+
+let selectedRating = null;
+
+function ratingSelected(e){
+    selectedRating = e.target.getAttribute("data-value");
+    if(selectedRating >= 1 && selectedRating <= 5){
+        outOf.textContent = selectedRating;
+    }
+}
+
+function changeCard(){
+    if(selectedRating){
+        cardRating.classList.add('hidden');
+        cardThanks.classList.remove('hidden');
+        cardThanks.classList.add('visible');
+    }
+}
+
+buttons.forEach(btn =>{
+    btn.addEventListener("click", ratingSelected);
+});
+
+btnSubmit.addEventListener('click', changeCard);
+
+
+
